6171cff92b365365aa5ebe330a47c06379907427
Robert Hevesi <roberthevesi09@gmail.com>
2025-01-28 21:04:29 +0200

    Initialized empty Spring project

---
+++ src/main/java/com/roberthevesi/easyURL/EasyUrlApplication.java
@@ -0,0 +1,13 @@
+package com.roberthevesi.easyURL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyUrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyUrlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roberthevesi/easyURL/EasyUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roberthevesi.easyURL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyUrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
